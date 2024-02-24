5e0dc67d7b0022d673289fdf7478d0e80711942e
Jared <jared.devenport@gmail.com>
2024-02-24 04:29:00 -0700

    enough

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
 import './App.css';
 import courses from './data/coursedata';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
 
+const Header = ({ collapseAllDropdowns, expandAllDropdowns, uncheckAllCheckboxes, checkDefaultCourses }) => {
+    return (
+        <header className="header">
+            <h1>CS Course Catalog</h1>
+            <div className="buttons">
+                <button onClick={collapseAllDropdowns}>Collapse All</button>
+                <button onClick={expandAllDropdowns}>Expand All</button>
+                <button onClick={uncheckAllCheckboxes}>Uncheck All</button>
+                <button onClick={checkDefaultCourses}>Default User</button>
+            </div>
+        </header>
+    );
+};
+
 const App = () => {
     const [courseStatus, setCourseStatus] = useState({});
+    const [allDropdownsOpen, setAllDropdownsOpen] = useState(false);
+    const [individualDropdownsOpen, setIndividualDropdownsOpen] = useState({});
+
+    useEffect(() => {
+        if (allDropdownsOpen) {
+            // If all dropdowns are open, set individual dropdowns open state accordingly
+            const dropdownsState = {};
+            courses.forEach((course) => {
+                dropdownsState[course.course_id] = true;
+            });
+            setIndividualDropdownsOpen(dropdownsState);
+        } else {
+            // If all dropdowns are collapsed, close all individual dropdowns
+            setIndividualDropdownsOpen({});
+        }
+    }, [allDropdownsOpen]);
 
     const handleCheckboxChange = (courseId) => {
         setCourseStatus((prevStatus) => {
             return {...prevStatus, [courseId]: !prevStatus[courseId]};
         });
     }
+    const collapseAllDropdowns = () => {
+        setAllDropdownsOpen(false);
+        setIndividualDropdownsOpen({});
+    };
+    const expandAllDropdowns = () => {
+        setAllDropdownsOpen(true);
+        setIndividualDropdownsOpen({});
+    };
+    const uncheckAllCheckboxes = () => {
+        setCourseStatus({});
+    };
+
+    const checkDefaultCourses = () => {
+        const defaultCourseIds = [142,235,224,236,240,260];
+        const defaultCourseStatus = defaultCourseIds.reduce((acc,courseId) => {
+            acc[courseId] = true;
+            return acc;
+        },{});
+        setCourseStatus(defaultCourseStatus);
+    }
+    const toggleDropdown = (courseId) => {
+        setIndividualDropdownsOpen((prevState) => ({
+            ...prevState,
+            [courseId]: !prevState[courseId],
+        }));
+    };
 
     return (
-        <div>
-            <h1>CS Course Catalog</h1>
+        <div className="page">
+            <Header
+                collapseAllDropdowns={collapseAllDropdowns}
+                expandAllDropdowns={expandAllDropdowns}
+                uncheckAllCheckboxes={uncheckAllCheckboxes}
+                checkDefaultCourses={checkDefaultCourses}
+            />
             <div className="course-list">
                 {courses.map((course) => (
                     <CourseList
@@ -24,6 +85,8 @@ const App = () => {
                         isChecked={courseStatus[course.course_id] || false}
                         onCheckboxChange={handleCheckboxChange}
                         courseStatus={courseStatus}
+                        isOpen={individualDropdownsOpen[course.course_id] || false}
+                        toggleDropdown={toggleDropdown}
                     />
                 ))}
             </div>
@@ -31,21 +94,16 @@ const App = () => {
     );
 }
 
-const CourseList = ({ course, isChecked, onCheckboxChange, courseStatus }) => {
+const CourseList = ({course, isChecked, onCheckboxChange, courseStatus, isOpen, toggleDropdown}) => {
     const filtered_title = course.title.replace(/^C S \d+:/, "");
-    const [isOpen, setIsOpen] = useState(false);
     const hasUnmetPrereqs = course.prereq_id && !courseStatus[course.prereq_id];
-
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
 
     return (
         <div className={`course-tile ${hasUnmetPrereqs ? 'unmet-prereq' : isChecked ? 'checked' : ''}`}>
             <div className="course-header">
                 <h1>CS {course.course_id}</h1>
                 <h2>{filtered_title}</h2>
-                <p>taken:</p>
+                <p>completed:</p>
                 <input
                     type="checkbox"
                     checked={isChecked}
@@ -53,14 +111,41 @@ const CourseList = ({ course, isChecked, onCheckboxChange, courseStatus }) => {
                 />
             </div>
             <p>Credits: {course.credits}</p>
-            <button type="button" onClick={toggleDropdown} className="dropdown-toggle">
+            <button
+                type="button"
+                onClick={() => toggleDropdown(course.course_id)}
+                className="dropdown-toggle"
+            >
                 <FontAwesomeIcon icon={faAngleDown} />
             </button>
             {isOpen && (
                 <div className="course-details">
-                    <p>Description: {course.course_description}</p>
+                    <h4>Description</h4>
+                    <p>{course.course_description}</p>
+                    <h4>Professors & Ratings:</h4>
+                    <div className="professor-details">
+                        <InstructorCardList instructors={course.instructors}/>
+                    </div>
                 </div>
             )}
+        </div>
+    );
+};
+
+const InstructorCardList = ({instructors}) => {
+   return (
+       <div className="instructor-card-list">
+           {instructors.map(instructor => (
+               <InstructorCard instructor={instructor}/>
+           ))}
+       </div>
+   );
+};
+
+const InstructorCard = ({instructor}) => {
+    return (
+        <div className="rmp-links">
+            <a href={instructor.rmp_link} rel="noreferrer" target="_blank">{instructor.first_name} {instructor.last_name}</a>
         </div>
     );
 };
